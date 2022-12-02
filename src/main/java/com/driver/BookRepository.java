ffa94ec698c210112706f61fe84aa9d22fc8b1ab
Code-WithSachin <koresachin1999@gmail.com>
2022-12-03 00:00:49 +0530

    first attempt

--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -1,41 +1,65 @@
 package com.driver;
 import java.util.*;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+@Component
 @Repository
 public class BookRepository {
 
+    HashMap<Integer, Book> bookDataBase=new HashMap<>();
+
     public BookRepository(){
-        
+
     }
 
     public Book save(Book book){
-        return null;
+        int id=bookDataBase.size()+1;
+        book.setId(id);
+        bookDataBase.put(id, book);
+        return book;
     }
 
     public Book findBookById(int id){
-
-        return null;
+        return bookDataBase.get(id);
     }
 
     public List<Book> findAll(){
-        return null;
+        List<Book> listOfBooks=new ArrayList<>();
+        for(Book book:bookDataBase.values()){
+            listOfBooks.add(book);
+        }
+        return listOfBooks;
     }
 
     public void deleteBookById(int id){
+        bookDataBase.remove(id);
         return;
     }
 
     public void deleteAll(){
+        bookDataBase.clear();
         return;
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book> booksByAuthorName=new ArrayList<>();
+        for(Book book:bookDataBase.values()){
+            if(book.getAuthor().equals(author)){
+                booksByAuthorName.add(book);
+            }
+        }
+        return booksByAuthorName;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+        List<Book> booksByGenre=new ArrayList<>();
+        for(Book book:bookDataBase.values()){
+            if(book.getGenre().equals(genre)){
+                booksByGenre.add(book);
+            }
+        }
+        return booksByGenre;
     }
 }
